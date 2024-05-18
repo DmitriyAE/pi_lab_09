c799800d70f282fdfa1679f0dd4be903bd89ae9c
Dmitry_E <ef_dim@mail.ru>
2024-05-18 10:49:23 +0500

    replacing selectbox with number_input

--- main.py
+++ main.py
@@ -54,28 +54,30 @@ with open('data.csv') as file:
 # Визуализируем решение
 st.image('titaniс.jpg')
 st.subheader('Средний возраст пассажиров парохода «Титаник» по каждому классу обслуживания в зависимости от количества родственников (братьев, сестер, сводных братьев, сводных сестер, супругов на борту).')
-family_size_options = ['Без учета родственников', '0', '1', '2', '3', '4', '5', '6', '7', '8']
 class_service = ['1 класс', '2 класс', '3 класс']
-selected_family_size = st.selectbox('Для просмотра данных выберите значение поля SibSp:', family_size_options)
+selected_family_size = st.number_input('Для просмотра данных выберите значение поля SibSp:', 0, 8)
+family_size_options = st.checkbox('Без учета родственников')
 
-# Заполняем таблицу в зависимости от выбора selected_family_size
-if selected_family_size == 'Без учета родственников':
+# Заполняем таблицу и строим график в зависимости от выбора family_size_options
+if family_size_options:
     class1_avg, class2_avg, class3_avg = calculate_average_age(lines)
     st.table({'Класс обслуживания': class_service, 'Средний возраст': [class1_avg, class2_avg, class3_avg]})
-else:
-    family_size = int(selected_family_size)
-    class1_avg, class2_avg, class3_avg = calculate_average_age(lines, consider_family_size=True, family_size=family_size)
-    st.table({'Класс обслуживания': class_service, 'Средний возраст': [class1_avg, class2_avg, class3_avg]})
-
-# Строим столбчатую диаграмму в зависимости от выбора selected_family_size
-plt.figure(figsize=(8, 4))
-if selected_family_size == 'Без учета родственников':
+    plt.figure(figsize=(8, 4))
     plt.bar(class_service, [class1_avg, class2_avg, class3_avg])
     plt.xlabel('Класс пассажира (Pclass)')
     plt.ylabel('Средний возраст')
-    plt.title(f'Средний возраст пассажиров по классам обслуживания')
+    plt.title('Средний возраст пассажиров по классам обслуживания')
     st.pyplot(plt)
 else:
+    if selected_family_size == 'Без учета родственников':
+        class1_avg, class2_avg, class3_avg = calculate_average_age(lines)
+        st.table({'Класс обслуживания': class_service, 'Средний возраст': [class1_avg, class2_avg, class3_avg]})
+    else:
+        family_size = int(selected_family_size)
+        class1_avg, class2_avg, class3_avg = calculate_average_age(lines, consider_family_size=True, family_size=family_size)
+        st.table({'Класс обслуживания': class_service, 'Средний возраст': [class1_avg, class2_avg, class3_avg]})
+
+    plt.figure(figsize=(8, 4))
     plt.bar(class_service, [class1_avg, class2_avg, class3_avg])
     plt.xlabel('Класс пассажира (Pclass)')
     plt.ylabel('Средний возраст')
